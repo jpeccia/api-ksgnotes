48c10493a4ec701faec0225b0f355fddbe37c657
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2023-10-24 19:32:58 -0300

    get note tags

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -79,7 +79,16 @@ class NotesController {
         .orderBy("title");
     }
 
-    return response.json(notes);
+    const userTags = await knex("tags").where({ user_id });
+    const notesWithTags = notes.map((note) => {
+      const noteTags = userTags.filter((tag) => tag.note_id);
+      return {
+        ...note,
+        tags: noteTags,
+      };
+    });
+
+    return response.json(notesWithTags);
   }
 }
 
