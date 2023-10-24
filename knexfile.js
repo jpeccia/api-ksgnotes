ae24a19748797d7ffce5052c75469d3b51b6a792
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2023-10-24 16:44:43 -0300

    migrations para link e tag

--- knexfile.js
+++ knexfile.js
@@ -7,9 +7,18 @@ module.exports = {
     connection: {
       filename: path.resolve(__dirname, "src", "database", "database.db"),
     },
-    migrations:{
-      directory: path.resolve(__dirname, "src", "database", "knex", "migrations")
+    pool: {
+      afterCreate: (conn, cb) => conn.run("PRAGMA foreign_keys = ON", cb),
     },
-    useNullAsDefault: true
+    migrations: {
+      directory: path.resolve(
+        __dirname,
+        "src",
+        "database",
+        "knex",
+        "migrations"
+      ),
+    },
+    useNullAsDefault: true,
   },
 };
