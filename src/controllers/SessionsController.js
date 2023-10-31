cdb9157017eb8501da4aef545abf801ca8d049bc
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2023-10-31 18:00:56 -0300

    feat: create user validation

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,6 +1,15 @@
+const knex = require("../database/knex");
+const AppError = require("../utils/AppError");
+
 class SessionsControllers {
-  async create() {
+  async create(request, response) {
     const { email, password } = request.body;
+
+    const user = await knex("users").where({ email }).first();
+
+    if (!user) {
+      throw new AppError("Email e/ou senha incorreta", 401);
+    }
 
     return response.json({ email, password });
   }
