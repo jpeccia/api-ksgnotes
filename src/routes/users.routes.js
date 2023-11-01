18b3da30c02e199198075e03f14f68051e596521
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2023-11-01 12:59:23 -0300

    feat: database img save

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,14 +1,16 @@
 const { Router } = require("express");
 const multer = require("multer");
-const uploadConfig = require("../configs/upload")
+const uploadConfig = require("../configs/upload");
 
 const UsersControllers = require("../controllers/UsersControllers");
+const UserAvatarController = require("../controllers/UserAvatarController");
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
 const usersRoutes = Router();
 const upload = multer(uploadConfig.MULTER);
 
 const usersControllers = new UsersControllers();
+const userAvatarController = new UserAvatarController();
 
 usersRoutes.post("/", usersControllers.create);
 usersRoutes.put("/", ensureAuthenticated, usersControllers.update);
@@ -16,10 +18,7 @@ usersRoutes.patch(
   "/avatar",
   ensureAuthenticated,
   upload.single("avatar"),
-  (request, response) => {
-    console.log(request.file.filename);
-    response.json();
-  }
+  userAvatarController.update
 );
 
 module.exports = usersRoutes;
