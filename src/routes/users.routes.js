a1d91cc58cf773cfaf1a644632e58df8bf74832c
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2023-11-01 12:48:54 -0300

    feat: img avatar upload

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,13 +1,25 @@
 const { Router } = require("express");
+const multer = require("multer");
+const uploadConfig = require("../configs/upload")
 
 const UsersControllers = require("../controllers/UsersControllers");
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
 const usersRoutes = Router();
+const upload = multer(uploadConfig.MULTER);
 
 const usersControllers = new UsersControllers();
 
 usersRoutes.post("/", usersControllers.create);
 usersRoutes.put("/", ensureAuthenticated, usersControllers.update);
+usersRoutes.patch(
+  "/avatar",
+  ensureAuthenticated,
+  upload.single("avatar"),
+  (request, response) => {
+    console.log(request.file.filename);
+    response.json();
+  }
+);
 
 module.exports = usersRoutes;
