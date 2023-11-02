77403ba045bbad01ac00053de29817086df8c73c
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2023-11-02 14:31:56 -0300

    fix: remove duplicated tags

--- src/controllers/UsersControllers.js
+++ src/controllers/UsersControllers.js
@@ -31,7 +31,9 @@ class UsersControllers {
     const user_id = request.user.id;
 
     const database = await sqliteConnection();
-    const user = await database.get("SELECT * FROM users WHERE id = (?)", [user_id]);
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [
+      user_id,
+    ]);
 
     if (!user) {
       throw new AppError("Usuário não encontrado");
