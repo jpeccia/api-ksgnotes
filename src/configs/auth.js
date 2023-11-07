dcc1af3c7773c12a8fea181f02f826ba1e8ce35f
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2023-11-07 17:31:12 -0300

    build: install dotenv

--- src/configs/auth.js
+++ src/configs/auth.js
@@ -1,6 +1,6 @@
 module.exports = {
   jwt: {
-    secret: "notesksgdefault",
+    secret: process.env.AUTH_SECRET || "default",
     expiresIn: "1d",
   },
 };
--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+require("dotenv/config");
 require("express-async-errors");
 
 const migrationsRuns = require("./database/sqlite/migrations");
@@ -33,5 +34,5 @@ app.use((error, request, response, next) => {
   });
 });
 
-const PORT = 3333;
+const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
